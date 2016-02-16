049bc9b51bbe687b703d77f69852ba02162536cf
Endy Muhardin <endy.muhardin@gmail.com>
2016-02-16 14:44:51 +0700

    Implement simpan data peserta baru

--- aplikasi-training-springboot/src/main/java/com/brainmatics/training/springboot/controller/PesertaController.java
+++ aplikasi-training-springboot/src/main/java/com/brainmatics/training/springboot/controller/PesertaController.java
@@ -3,10 +3,13 @@ package com.brainmatics.training.springboot.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.brainmatics.training.springboot.dao.PesertaDao;
 import com.brainmatics.training.springboot.entity.Peserta;
@@ -19,5 +22,11 @@ public class PesertaController {
 	@ResponseBody
 	public Page<Peserta> semuaPeserta(Pageable page){
 		return pesertaDao.findAll(page);
+	}
+	
+	@RequestMapping(value="/peserta/", method=RequestMethod.POST)
+	@ResponseStatus(HttpStatus.CREATED)
+	public void pesertaBaru(@RequestBody Peserta peserta){
+		pesertaDao.save(peserta);
 	}
 }
--- aplikasi-training-springboot/src/test/java/com/brainmatics/training/springboot/controller/PesertaControllerTest.java
+++ aplikasi-training-springboot/src/test/java/com/brainmatics/training/springboot/controller/PesertaControllerTest.java
@@ -1,8 +1,8 @@
 package com.brainmatics.training.springboot.controller;
 
-import static com.jayway.restassured.RestAssured.get;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasItems;
+import static com.jayway.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import java.util.Date;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -14,7 +14,9 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.brainmatics.training.springboot.AplikasiTrainingSpringbootApplication;
+import com.brainmatics.training.springboot.entity.Peserta;
 import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(AplikasiTrainingSpringbootApplication.class)
@@ -35,5 +37,21 @@ public class PesertaControllerTest {
 		.then()
 		.body("totalElements", equalTo(9))
 		.body("content.id", hasItems(91, 92));
+	}
+	
+	@Test
+	public void testSimpanPesertaBaru(){
+		Peserta p = new Peserta();
+		p.setKode("PT-001");
+		p.setNama("Peserta Test 001");
+		p.setTanggalLahir(new Date());
+		
+		given()
+			.body(p)
+			.contentType(ContentType.JSON)
+		.when()
+			.post("/peserta/")
+		.then()
+			.statusCode(201);
 	}
 }
