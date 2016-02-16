b52f931c5be5d95ede1fd0389bcea90bf8c25e99
Endy Muhardin <endy.muhardin@gmail.com>
2016-02-16 10:17:00 +0700

    implement dao dengan Spring Data JPA

---
+++ aplikasi-training-springboot/src/test/java/com/brainmatics/training/springboot/dao/PesertaDaoTest.java
@@ -0,0 +1,53 @@
+package com.brainmatics.training.springboot.dao;
+
+import java.util.Date;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.brainmatics.training.springboot.AplikasiTrainingSpringbootApplication;
+import com.brainmatics.training.springboot.entity.Peserta;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(AplikasiTrainingSpringbootApplication.class)
+@Sql(scripts="/sample-peserta.sql")
+public class PesertaDaoTest {
+	@Autowired
+	private PesertaDao pesertaDao;
+	
+	@Test
+	public void testSimpan(){
+		Peserta p = new Peserta();
+		p.setNama("Endy");
+		p.setKode("P-002");
+		p.setTanggalLahir(new Date());
+		
+		Assert.assertNull(p.getId());
+		pesertaDao.save(p);
+		Assert.assertNotNull(p.getId());
+	}
+	
+	@Test
+	public void testCariById(){
+		Integer id = 99;
+		Peserta px = pesertaDao.findOne(id);
+		Assert.assertNotNull(px);
+		Assert.assertEquals("P-001", px.getKode());
+		Assert.assertEquals("Peserta 001", px.getNama());
+		
+		Assert.assertNull(pesertaDao.findOne(101));
+		
+	}
+	
+	@Test
+	public void testDeleteById(){
+		Assert.assertNotNull(pesertaDao.findOne(99));
+		pesertaDao.delete(99);
+		Assert.assertNull(pesertaDao.findOne(99));
+	}
+}
---
+++ aplikasi-training-springboot/src/test/resources/sample-peserta.sql
@@ -0,0 +1,3 @@
+delete from peserta where id = 99;
+insert into peserta (id, kode, nama, tanggal_lahir) values 
+(99, 'P-001', 'Peserta 001', '1945-08-17');
