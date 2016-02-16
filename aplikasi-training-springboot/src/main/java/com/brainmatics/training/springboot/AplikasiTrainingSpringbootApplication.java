6b084d784e676356f9ef8ed3bcd9c5964d9a43c6
Endy Muhardin <endy.muhardin@gmail.com>
2016-02-16 09:19:02 +0700

    starter project dari start.spring.io

---
+++ aplikasi-training-springboot/src/main/java/com/brainmatics/training/springboot/AplikasiTrainingSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.brainmatics.training.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikasiTrainingSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplikasiTrainingSpringbootApplication.class, args);
+	}
+}
