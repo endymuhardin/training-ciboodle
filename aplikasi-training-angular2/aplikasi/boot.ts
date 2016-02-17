e412d3bfb716678a1c2ba6d18b791ae5c11c20c3
Endy Muhardin <endy.muhardin@gmail.com>
2016-02-17 11:37:11 +0700

    implement komponen sederhana

--- aplikasi-training-angular2/aplikasi/boot.ts
+++ aplikasi-training-angular2/aplikasi/boot.ts
@@ -1,1 +1,6 @@
+import 'reflect-metadata';
+import {bootstrap} from 'angular2/platform/browser';
+import {HaloComponent} from './halo/halo.component';
+
 console.log("Ini file boot.ts");
+bootstrap(HaloComponent);
---
+++ aplikasi-training-angular2/aplikasi/halo/halo.component.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'halo',
+  template: `<h2>Halo Component</h2>`
+})
+export class HaloComponent {
+    
+}
