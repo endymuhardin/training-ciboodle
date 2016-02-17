e6edb6fa153aa6e9a44f18ec3ee7df85d6c8664a
Endy Muhardin <endy.muhardin@gmail.com>
2016-02-17 13:19:11 +0700

    pisahkan template ke file sendiri

--- aplikasi-training-angular2/aplikasi/halo/halo.component.ts
+++ aplikasi-training-angular2/aplikasi/halo/halo.component.ts
@@ -2,7 +2,7 @@ import {Component} from 'angular2/core';
 
 @Component({
   selector: 'halo',
-  template: `<h2>Halo Component</h2>`
+  templateUrl: './aplikasi/halo/halo.component.html'
 })
 export class HaloComponent {
     
